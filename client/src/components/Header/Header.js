c7f88154d3578f13ffad5759c075900137fc8c01
Vinson Ridenour <vinson.ridenour@gmail.com>
2017-09-16 13:25:02 -0700

    antd designing

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
-
+import { Button } from 'antd';
 import 'antd/dist/antd.css';
 import "./Header.css";
 
 class Header extends Component {
 
     constructor() {
-
       super();
 
     //   this.state = {
     //     isActive: false
     //   };
 
-    }
+    this.state = {
+        size: 'large',
+      };
+    };
 
     render() {
+        const size = this.state.size;
 		return (
             <header>
                 <nav>
                     <ul>
-                        <li className="logo"><a href="#"><i className="fa fa-file-code-o" aria-hidden="true"></i></a></li>
+                        <li className="logo"><a href="#"><i className="fa fa-file-code-o"></i></a></li>
                         <li><a href="#">About></a></li>
                         <li><a href="#">Contact></a></li>
                     </ul>
                 </nav>
-                <button>Login</button>
+                <Button type="primary" size={size}>Login</Button>
             </header>
         )
     }
