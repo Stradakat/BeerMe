9ddc62eaf4467c3cef62ed03cf4d9b05b4ec3401
chesteraustin <chesteraustin@gmail.com>
2017-09-16 13:18:08 -0700

    require server modules

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+// Include Server Dependencies
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+require('dotenv').config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Serve up static assets if in production (running on Heroku)
 if (process.env.NODE_ENV === "production") {
@@ -10,9 +19,9 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//API routes
+const routes = require("./server/controllers/API");
+app.use("/", routes);
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
