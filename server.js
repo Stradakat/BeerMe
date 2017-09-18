b7878dfec2691dc70480e5250742cdbcea2cacfe
chesteraustin <chesteraustin@gmail.com>
2017-09-18 14:07:57 -0700

    allow CORS in server.js

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+//Allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // Serve up static assets if in production (running on Heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
