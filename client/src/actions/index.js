c32e62ecfbdfdaf0a57967dc49dee5acf9928688
Vinson Ridenour <vinson.ridenour@gmail.com>
2017-09-22 15:38:04 -0700

    more auth progress

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { firebase, googleAuthProvider } from '../firebase/firebase';
 export const REQUEST_BEERS = 'REQUEST_BEERS';
 export const RECEIVE_BEERS = 'RECEIVE_BEERS';
 export const REQUEST_BREWERY = 'REQUEST_BREWERY';
@@ -23,3 +24,41 @@ export const receiveBrewery = (user) => ({
   "type": RECEIVE_BREWERY,
   user
 })
+
+// start login authentication
+export const startLogin = () => {
+  return () => {
+      return firebase.auth().signInWithPopup(googleAuthProvider)
+      .then((result) => {
+        // const token = result.credential.accessToken
+        // console.log(token);
+        window.location.href = '/reclist';
+      }).catch((e) => {
+        console.log('Unable to log in!', e);
+      });
+  };
+};
+
+// start logout
+export const startLogout = () => {
+  return () => {
+      return firebase.auth().signOut()
+      .then((result) => {
+        window.location.href = '/';
+      });
+  };
+};
+
+export const login = (uid) => {
+  return {
+      type: 'LOGIN',
+      uid: uid
+  }
+}
+
+export const logout = (uid) => {
+  return {
+      type: 'LOGOUT',
+      uid: uid
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,25 +3,27 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { routes, store, history, middleware } from './config/routes';
 import { firebase } from './firebase/firebase';
-import { login, logout } from './actions/auth';
+import { login, logout } from './actions/index';
 
 
 ReactDOM.render(routes, document.getElementById('root'));
 
-// fire everytime auth state changes, including first load of app
+// fire everytime auth state changes and on initial app load
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+        console.log(user.uid)
         store.dispatch(login(user.uid));
-        // route user to the reclist page if they're on homepage and authenticated
-        ReactDOM.render(routes, document.getElementById('root'));
-        if (history.location.pathname === '/') {
-            history.push('/reclist')
-        }
+        // window.location.href = '/reclist'
+        // history.push('/reclist');
+        // ReactDOM.render(routes, document.getElementById('root'));
+        
     } else {
-        // console.log('log out');
         store.dispatch(logout(user.uid));
+        if (window.location.pathname !== '/') {
+            window.location.href = "/";
+        }
         ReactDOM.render(routes, document.getElementById('root'));
-        history.push('/');
+        // history.push('/');
     }
 });
 
