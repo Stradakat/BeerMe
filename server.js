8620207a5860b3e963d284313652e969658d954f
chesteraustin <chesteraustin@gmail.com>
2017-09-21 19:29:01 -0700

    enable mongoose connections

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Include Server Dependencies
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const dotenv = require('dotenv').config();
 const app = express();
 require('dotenv').config();
 
@@ -18,6 +18,27 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
+
+//                                              Mongo & Mongoose Connections
+const mongoose = require("mongoose");
+
+//Schemas to be used
+var Survey = require("./server/models/Survey.js");
+
+// Database configuration with mongoose
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("openURI", function() {
+  console.log("Mongoose connection successful.");
+});
+
 
 // Serve up static assets if in production (running on Heroku)
 if (process.env.NODE_ENV === "production") {
