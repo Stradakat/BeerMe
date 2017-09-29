87509f8ca58badcfa8fc714d5ebd34e58b4e45e3
chesteraustin <chesteraustin@gmail.com>
2017-09-29 12:21:41 -0700

    brewery card with AJAX updates

--- client/src/components/Brewery/Brewery.js
+++ client/src/components/Brewery/Brewery.js
@@ -18,26 +18,31 @@ class Brewery extends Component {
 	}
 	componentDidMount() {
 		console.log("component mounted")
-		this.props.getBrewery("nMaqhu");
+//		this.props.getBrewery("nMaqhu");
+    }
+
+	updateBrewery(breweryID) {
+		this.props.getBrewery(breweryID);
     }
 
     createCards() {
         let brewCards = [];
         let breweryList = [
-                            {"breweryID": "puznP2",
-                                "name": "brewery 1",
-                                "image": "image"},
+                            {"breweryID": "Nj8cgD",
+                                "name": "Green Flash",
+                                "image": "https://s3.amazonaws.com/brewerydbapi/brewery/Nj8cgD/upload_8QinVn-large.png"},
                             {"breweryID": "ygAzC9",
-                                "name": "brewery 2",
-                                "image": "image"}
+                                "name": "AleSmith",
+                                "image": "https://s3.amazonaws.com/brewerydbapi/brewery/ygAzC9/upload_KpBT5m-large.png"}
                         ]
         for(var i = 0; i < breweryList.length; i++) {
             let brewRec = breweryList[i]
             brewCards.push(
                 <div key={i}>
-                    <Card bordered={true}>
+                    <Card bordered={true} onClick={() => this.updateBrewery(brewRec.breweryID)}>
                         <div>
-                            <img className="brewImg" src={brewRec.img} alt={brewRec.name}></img>
+                            {console.log(brewRec)}
+                            <img className="brewImg" src={brewRec.image} alt={brewRec.name}></img>
                         </div>
                     </Card>
                 </div>
@@ -49,29 +54,28 @@ class Brewery extends Component {
     render() {
         return (
             <div className="entire-page">
-                {console.log(this.props.breweryDetails)}
                 <Header />
                 <div className="empty-space"></div>
                 <div className="brewRow">
                     {this.createCards()}
                 </div>
-                    <div className="all-content-container">
-                        <div className="brew-image-container">
-                            <img className="brew-image" src={this.props.breweryDetails.images.large} alt="brewery"/>
+                <div className="all-content-container">
+                    <div className="brew-image-container">
+                        <img className="brew-image" src={this.props.breweryDetails.images.large} alt="brewery"/>
+                    </div>
+                    <div className="brew-text-container">
+                        <div className="brewery-name-container">
+                            <h1 className="brewery-name">{this.props.breweryDetails.name}</h1>
                         </div>
-                        <div className="brew-text-container">
-                            <div className="brewery-name-container">
-                                <h1 className="brewery-name">{this.props.breweryDetails.name}</h1>
-                            </div>
-                            <div className="brewery-desc-container">
-                                <h2 className="brewery-desc">{this.props.breweryDetails.description}</h2>
-                            </div>
-                            <div className="brewery-site-container">
-                                <a className="brewery-site-name" href={this.props.breweryDetails.website}>{this.props.breweryDetails.website}</a>
-                            </div>
+                        <div className="brewery-desc-container">
+                            <h2 className="brewery-desc">{this.props.breweryDetails.description}</h2>
+                        </div>
+                        <div className="brewery-site-container">
+                            <a className="brewery-site-name" href={this.props.breweryDetails.website}>{this.props.breweryDetails.website}</a>
                         </div>
                     </div>
-                    <Footer />
+                </div>
+                <Footer />
             </div>
         );
     }
